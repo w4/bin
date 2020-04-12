53d97709faa378c5acf1c4367f8cdce2936818b2
Jordan Doyle <jordan@doyle.la>
2020-04-12 16:07:10 +0100

    Bump package versions

--- shell.nix
+++ shell.nix
@@ -2,8 +2,8 @@ with import <nixpkgs> {};
 let src = fetchFromGitHub {
       owner = "mozilla";
       repo = "nixpkgs-mozilla";
-      # commited 12/2/2019
-      rev = "37f7f33ae3ddd70506cd179d9718621b5686c48d";
+      # commited 19/2/2020
+      rev = "e912ed483e980dfb4666ae0ed17845c4220e5e7c";
       sha256 = "0cmvc9fnr38j3n0m4yf0k6s2x589w1rdby1qry1vh435v79gp95j";
    };
 in
@@ -14,6 +14,9 @@ stdenv.mkDerivation {
     buildInputs = [
         latest.rustChannels.nightly.cargo
         latest.rustChannels.nightly.rust
+        stdenv.cc.libc
     ] ++
     pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
+
+    LIBCLANG_PATH="${llvmPackages.libclang}/lib";
 }
